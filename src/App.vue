6aadd38275e7677fe8e858079e75492e77e3022b
Szqii <szqii.altn@gmail.com>
2025-09-09 20:41:24 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import DefaultLayout from "@/layout/DefaultLayout.vue";
+import {useAppointmentsStore} from "@/stores/appointments.js";
+import {onMounted} from "vue";
+
+const appointmentStore = useAppointmentsStore();
+
+onMounted(() => {
+  appointmentStore.fetchAppointments();
+})
+</script>
+
+<template>
+  <DefaultLayout class="bg-[#F9F9F9]"/>
+</template>
+
+<style scoped></style>
