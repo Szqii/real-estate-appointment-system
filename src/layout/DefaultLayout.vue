b90381c6b6e3f76cfdade038835ea36ac26f9a5f
Szqii <szqii.altn@gmail.com>
2025-09-10 10:31:28 +0300

    add(layout): footer

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="app-container">
-    <header class="app-header">
-      <h1>Appointments</h1>
-    </header>
-    <main class="app-main">
+  <div class="app-container flex flex-col min-h-screen px-8">
+    <main class="app-main flex-grow pb-6">
       <router-view/>
     </main>
-    <footer class="app-footer">
-      <p>&copy; Real Estate Scheduler</p>
+    <footer class="app-footer text-center py-4 mt-auto border-t">
+      <p>&copy; Iceberg Digital | Real Estate Appointment System</p>
     </footer>
   </div>
 
