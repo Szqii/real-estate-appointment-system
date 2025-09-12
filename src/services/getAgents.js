30b55bd8817d42d605ea38666646c533bfb8785a
Szqii <szqii.altn@gmail.com>
2025-09-12 16:42:59 +0300

    refactor: remove console.log

--- src/services/getAgents.js
+++ src/services/getAgents.js
@@ -1,12 +1,11 @@
 import instance from '@/services/index.js'
 
 export const getAgents = async (params) => {
-  try {
-    const response = await instance.get('/agents', { params })
-    console.log(response.data)
-    return response.data.records
-  } catch (error) {
-    console.error('Error fetching appointments:', error)
-    throw error
-  }
+    try {
+        const response = await instance.get('/agents', {params})
+        return response.data.records
+    } catch (error) {
+        console.error('Error fetching appointments:', error)
+        throw error
+    }
 }
